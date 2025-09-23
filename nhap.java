df3f468ff684eb5a6eb4adc5a76ad0c1ae16d03c
hoangvu205 <11convittroi@gmail.com>
2025-09-23 23:13:11 +0700

    nhap.java

--- nhap.java
+++ nhap.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
+
 public class nhap{
     public static void main(String[] args) {
-        System.out.println("day la pj java");
+        JFrame windown=new JFrame("cua so app");
+        windown.setSize(500,500);
+        windown.setDefaultCloseOperation(windown.EXIT_ON_CLOSE);
+        JButton view=new JButton("view");
+        windown.setLayout(null);
+        JButton chuyen=new JButton("chuyen");
+        chuyen.setBounds(2,2,220,20);
+        windown.add(chuyen);
+        windown.setVisible(true);
     }
 }
